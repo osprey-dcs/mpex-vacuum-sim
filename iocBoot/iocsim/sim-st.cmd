dcf5a482c056c03f2f8b0b6dd6c4b5dffb4c6f15
Michael Dalesio <michael.dalesio@gmail.com>
2025-12-09 16:51:19 -0500

    Updates st.cmd, Release, and Db/Make
    Cleans up st.cmd file, and adds autosave, iocstats, and reccaster

--- iocBoot/iocsim/sim-st.cmd
+++ iocBoot/iocsim/sim-st.cmd
@@ -5,10 +5,26 @@
 
 < envPaths
 
-#load header info common to all IOCs
-< header-st.cmd
+epicsEnvSet("PREFIX", "VAC_SIM")
+epicsEnvSet("IOCNAME", "mpex-vacuum-sim")
+epicsEnvSet("IOC", "iocsim")
+epicsEnvSet("AS_PATH", "$(TOP)/iocBoot/$(IOC)/autosave")
 
-#Variables unique to this IOC
+## Register all support components
+dbLoadDatabase "../../dbd/sim.dbd"
+sim_registerRecordDeviceDriver(pdbbase) 
+
+## Load record instances and send port "name" as parameter
+dbLoadRecords("../../db/sim.db")
+
+## Initialize EtherIP driver, define PLCs
+drvEtherIP_init()
+
+## Set AutoSave paths
+set_requestfile_path("$(AS_PATH)")
+set_savefile_path("$(AS_PATH)")
+
+## Variables unique to this IOC
 #epicsEnvSet("PLCIP","10.112.8.13")
 #epicsEnvSet("PLCNAME","vcs")
 #epicsEnvSet("PLCSLOT", 4)
@@ -16,10 +32,17 @@
 # Attach port "name" to the IP and CPU slot number
 #drvEtherIP_define_PLC($(PLCNAME), $(PLCIP), $(PLCSLOT))
 
-## Load record instances and send port "name" as parameter
-dbLoadRecords("../../db/sim.db")
+## Reccaster db
+dbLoadRecords("$(TOP)/db/reccaster.db", "P=$(PREFIX):RecSync")
 
-#load tail info common to all IOCs
-< tail-st.cmd
+## IOCSTATS db
+dbLoadRecords("$(TOP)/db/iocAdminSoft.db", "IOC=$(PREFIX):STS")
+
+## Access security file to load
+#asSetFilename("${ASCF}/ics-default.acf")
 
 iocInit()
+
+save_restoreSet_Debug(0)
+makeAutosaveFileFromDbInfo("$(AS_PATH)/info_settings.req", "autosaveFields")
+create_monitor_set("info_settings.req", 30, "")
--- iocBoot/iocsim/tail-st.cmd
+++
@@ -1,8 +0,0 @@
-#Reccaster db
-dbLoadRecords("$(TOP)/db/reccaster.db", "P=$(IOCNAME)-$(PLCNAME):RecSync")
-
-#IOCSTATS db
-dbLoadRecords("$(TOP)/db/iocAdminSoft.db", "IOC=$(IOC)-$(PLCNAME)")
-
-#Access security file to load
-#asSetFilename("${ASCF}/ics-default.acf")
